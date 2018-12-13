bee0bfc780aeb6919fbc664a8e13bbb88f2afc55
Miles <41974447+bm20894@users.noreply.github.com>
2018-12-13 15:34:53 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-# main.py
+'''
+Programmer: Miles Boswell
+main.py
+
+Take user input from cli (image file, tool to apply on image)
+Display original image and transformed image side by side in window
+Optional: overwrite image file with transformation
+'''
+import sys
+from matplotlib import pyplot as plt
+from PIL import Image
+from utils import error, transform
+
+def user_input():
+	''' @return: dictionary with Image object as 'img',
+				 file of Image as 'file', and optional tool
+				 to apply '''
+	if len(sys.argv) > 1:
+		sys.argv.pop(0)
+		data = {}
+		if '-t' in sys.argv:
+			tool_index = sys.argv.index('-t') + 1
+			tool = sys.argv.pop(tool_index)
+			data['tool'] = tool
+			sys.argv.pop(tool_index - 1)
+		image_file = sys.argv[0]
+		try:
+			img = Image.open(image_file)
+		except:
+			error('Could not find file: {}'.format(image_file))
+		else:
+			data['img'], data['file'] = img, image_file
+		return data
+	else:
+		error('You must specify an image file.')
+
+if __name__ == '__main__':
+	data = user_input()
+	print(data)
+	# if a tool is specified, apply it to the Image and display
+	img, tool = data.get('img'), data.get('tool', None)
+	if tool:
+		trans = transform(tool)
+		trans(img)
