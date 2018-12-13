9f2fe98c956a86c528b59f48a285957a30d2d864
Miles <41974447+bm20894@users.noreply.github.com>
2018-12-13 15:36:12 -0600

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,14 @@
+'''
+Util package for main.py
+'''
+import sys
+from termcolor import cprint
+import tools
 
+def error(msg):
+	cprint(msg, 'red')
+	sys.exit()
+
+def transform(tool):
+	'''@return: function to execute on Image object'''
+	return lambda img: img.show()
