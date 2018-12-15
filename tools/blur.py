2af49ad677b1b9e69cacd679db680043817322c8
bm20894 <milesboz@me.com>
2018-12-14 20:18:40 -0600

    blur.py docstring

--- tools/blur.py
+++ tools/blur.py
@@ -2,6 +2,8 @@ from PIL import ImageFilter
 from os import path
 
 def blur(img, radius=3):
+	''' @params an Image object, and an optional radius kwarg for the Guassian Blur.
+		@return the transformed Image object, with the applied Gaussian Blur.'''
 	im1 = img.filter(ImageFilter.BLUR)
 	im2 = img.filter(ImageFilter.GaussianBlur(radius=radius))
 
